d73e4a863e584ab43cf1273893c5b1c3982c69fa
David Thrane Christiansen <david@davidchristiansen.dk>
2024-05-26 15:52:08 -0700

    Remove big-step semantics, statement optimizer, and proof

--- Imp/Stmt/BigStep.lean
+++ Imp/Stmt/BigStep.lean
@@ -65,167 +65,37 @@ Big-step semantics: `BigStep σ s σ'` means that running the program `s` in the
 termination with the final state `σ'`.
 -/
 inductive BigStep : Env → Stmt → Env → Prop where
-  | skip :
-    BigStep σ (imp {skip;}) σ
-  | seq :
-    BigStep σ s1 σ' → BigStep σ' s2 σ'' →
-    BigStep σ (imp{ ~s1 ~s2}) σ''
-  | assign :
-    e.eval σ = some v →
-    BigStep σ (imp {~x := ~e;}) (σ.set x v)
-  | ifTrue :
-    Truthy (c.eval σ) → BigStep σ s1 σ' →
-    BigStep σ (imp {if (~c) {~s1} else {~s2}}) σ'
-  | ifFalse :
-    Falsy (c.eval σ) → BigStep σ s2 σ' →
-    BigStep σ (imp {if (~c) {~s1} else {~s2}}) σ'
-  | whileTrue :
-    Truthy (c.eval σ) →
-    BigStep σ body σ' →
-    BigStep σ' (imp {while (~c) {~body}}) σ'' →
-    BigStep σ (imp {while (~c) {~body}}) σ''
-  | whileFalse :
-    Falsy (c.eval σ) →
-    BigStep σ (imp {while (~c) {~body}}) σ
-
-attribute [simp] BigStep.skip
 
 /--
 `swap` terminates, and the resulting environment contains swapped inputs.
 -/
-example : ∃σ', BigStep (Env.init 0 |>.set "x" 5 |>.set "y" 22) swap σ' ∧ σ'.get "x" = 22 ∧ σ'.get "y" = 5 := by
-  unfold swap
-  apply Exists.intro
-  constructor
-  . apply BigStep.seq
-    . apply BigStep.assign
-      simp [Expr.eval, Env.get, Env.set]
-      rfl
-    . apply BigStep.seq
-      . apply BigStep.assign
-        simp [Expr.eval, Env.get, Env.set]
-        rfl
-      . simp
-        apply BigStep.assign
-        simp [Expr.eval, Env.get, Env.set]
-        rfl
-  . simp [Env.get, Env.set]
+example : ∃σ', BigStep (Env.init 0 |>.set "x" 5 |>.set "y" 22) swap σ' ∧ σ'.get "x" = 22 ∧ σ'.get "y" = 5 := by sorry
 
 /--
 `swap` terminates, and the resulting environment contains swapped inputs. This proof is shorter.
 -/
-example : ∃σ', BigStep (Env.init 0 |>.set "x" 5 |>.set "y" 22) swap σ' ∧ σ'.get "x" = 22 ∧ σ'.get "y" = 5 := by
-  repeat' constructor
+example : ∃σ', BigStep (Env.init 0 |>.set "x" 5 |>.set "y" 22) swap σ' ∧ σ'.get "x" = 22 ∧ σ'.get "y" = 5 := by sorry
 
 /--
 `swap` terminates, and the resulting environment contains swapped inputs. This version works no
 matter what the input values are.
 -/
-example : ∃σ', BigStep (Env.init 0 |>.set "x" x |>.set "y" y) swap σ' ∧ σ'.get "x" = y ∧ σ'.get "y" = x  := by
-  repeat' constructor
+example : ∃σ', BigStep (Env.init 0 |>.set "x" x |>.set "y" y) swap σ' ∧ σ'.get "x" = y ∧ σ'.get "y" = x  := by sorry
 
 /--
 `min` computes the minimum of its inputs.
 -/
-example : ∃σ', BigStep (Env.init 0 |>.set "x" x |>.set "y" y) min σ' ∧ if x < y then σ'.get "min" = x else σ'.get "min" = y := by
-  unfold min
-  by_cases h : x < y
-  . apply Exists.intro; constructor
-    . apply BigStep.ifTrue
-      . simp [Expr.eval, Expr.BinOp.apply, Env.get, Env.set, *]; decide
-      . constructor; simp [Expr.eval, Env.get, Env.set]; rfl
-    . simp [Env.get, Env.set]
-      intro; contradiction
-  . apply Exists.intro; constructor
-    . apply BigStep.ifFalse
-      . simp [Expr.eval, Expr.BinOp.apply, Env.get, Env.set, *]
-      . constructor; simp [Expr.eval, Env.get, Env.set]; rfl
-    . simp [Env.get, Env.set]
-      intro; contradiction
+example : ∃σ', BigStep (Env.init 0 |>.set "x" x |>.set "y" y) min σ' ∧ if x < y then σ'.get "min" = x else σ'.get "min" = y := by sorry
 
 def loop := imp {while (1) {skip;}}
 
 /--
 `loop` is really an infinite loop - there is no final state that it can result in.
 -/
-theorem infinite_loop : ¬ BigStep σ loop σ' := by
-  generalize h' : loop = l
-  intro h
-  induction h <;> try (simp [loop, *] at *; done)
-  case whileTrue =>
-    simp [*]
-  case whileFalse cFalse =>
-    unfold loop at h'
-    cases h'
-    simp at cFalse
-    contradiction
+theorem infinite_loop : ¬ BigStep σ loop σ' := by sorry
 
 /-- Optimizing a program doesn't change its meaning -/
-theorem optimize_ok : BigStep σ s σ' → BigStep σ s.optimize σ' := by
-  intro h
-  induction h with simp only [optimize]
-  | «skip» => constructor
-  | seq s1 s2 ih1 ih2 =>
-    split
-    next eq2 =>
-      rw [eq2] at ih1
-      cases ih1; apply ih2
-    next eq1 eq2 =>
-      rw [eq1] at ih2
-      cases ih2; apply ih1
-    next =>
-      apply BigStep.seq ih1 ih2
-  | assign m =>
-    constructor
-    rw [← Expr.optimize_ok]
-    assumption
-  | ifTrue isTrue l ih =>
-    split
-    next isFalse =>
-      rw [Expr.optimize_ok] at isTrue
-      rw [isFalse] at isTrue
-      simp [Truthy, Expr.eval] at isTrue
-    next notFalse _isConst =>
-      apply ih
-    next =>
-      split
-      . assumption
-      . apply BigStep.ifTrue
-        . rw [← Expr.optimize_ok]
-          assumption
-        . assumption
-  | ifFalse isFalse l ih =>
-    split
-    next =>
-      apply ih
-    next nonZero isConst =>
-      rw [Expr.optimize_ok, isConst] at isFalse
-      simp at isFalse
-      contradiction
-    next =>
-      split
-      . simp [*]
-      . apply BigStep.ifFalse
-        . rw [← Expr.optimize_ok]
-          assumption
-        . assumption
-  | whileFalse =>
-    split <;> try simp
-    apply BigStep.whileFalse
-    rw [← Expr.optimize_ok]
-    assumption
-  | whileTrue isTrue bodyStep nextStep ih1 ih2 =>
-    split
-    next c isZero =>
-      rw [Expr.optimize_ok, isZero] at isTrue
-      simp at isTrue
-    next c isNotZero =>
-      apply BigStep.whileTrue
-      . rw [← Expr.optimize_ok]
-        assumption
-      . apply ih1
-      . simp [optimize] at ih2
-        assumption
+theorem optimize_ok : BigStep σ s σ' → BigStep σ s.optimize σ' := by sorry
 
 /--
 Run a program, with the depth of the recursive calls limited by the `Nat` parameter. Returns `none`
@@ -263,7 +133,8 @@ def run (σ : Env) (s : Stmt) : Nat → Option Env
 semantics. This is not total correctness - `run` could always return `none` - but it does increase
 confidence.
 -/
-theorem run_some_implies_big_step : run σ s n = some σ' → BigStep σ s σ' := by
+theorem run_some_implies_big_step : run σ s n = some σ' → BigStep σ s σ' := by sorry
+/-
   intro term
   induction σ, s, n using run.induct generalizing σ' <;> unfold run at term <;> simp_all
   case case3 σ n s1 s2 ih1 ih2 =>
@@ -305,3 +176,4 @@ theorem run_some_implies_big_step : run σ s n = some σ' → BigStep σ s σ' :
         exact oneStep
       . apply ih2
         exact loopStep
+-/
--- Imp/Stmt/Optimize.lean
+++ Imp/Stmt/Optimize.lean
@@ -6,27 +6,4 @@ namespace Imp.Stmt
 
 /-- Optimize a statement -/
 def optimize : Stmt → Stmt
-  | imp {skip;} => imp {skip;}
-  | imp {~s1 ~s2} =>
-    match s1.optimize, s2.optimize with
-    | imp {skip;}, s2' => s2'
-    | s1', imp {skip;} => s1'
-    | s1', s2' => imp {~s1' ~s2'}
-  | imp {if (~c) {~s1} else {~s2}} =>
-    let c' := c.optimize
-    match c' with
-    | .const 0 => s2.optimize
-    | .const _ => s1.optimize
-    | _ =>
-      let s1' := s1.optimize
-      let s2' := s2.optimize
-      if s1' = s2' then
-        s1'
-      else imp {if (~c') {~s1.optimize} else {~s2.optimize}}
-  | imp {while (~c) {~s}} =>
-    let c' := c.optimize
-    match c' with
-    | .const 0 => imp {skip;}
-    | _ => imp {while (~c') {~s.optimize}}
-  | imp {~x := ~e;} =>
-    imp {~x := ~e.optimize;}
+  | stmt => stmt
