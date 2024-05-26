0a5c9d2ba24d0342a9de6435e036d84f5fd08439
David Thrane Christiansen <david@davidchristiansen.dk>
2024-05-26 16:00:44 -0700

    Delete proofs for array filter

--- Filter/Array.lean
+++ Filter/Array.lean
@@ -33,22 +33,13 @@ where
   termination_by arr.size - i
 
 def All (p : α → Prop) (arr : Array α) : Prop :=
-  (i : Nat) → (lt : i < arr.size) →  p arr[i]
+  sorry
 
 @[simp]
-theorem all_empty (p : α → Prop) : All p #[] := fun i lt =>
-  by contradiction
+theorem all_empty (p : α → Prop) : All p #[] := sorry
 
 @[simp]
-theorem push_all (hAll : All p xs) (hx : p x) : All p (xs.push x) := by
-  intros
-  intro i lt
-  by_cases i = xs.size
-  · simp [*]
-  · simp at lt
-    have : i < xs.size := by omega
-    simp [Array.get_push_lt, *]
-    apply hAll
+theorem push_all (hAll : All p xs) (hx : p x) : All p (xs.push x) := by sorry
 
 
 /-
@@ -66,22 +57,10 @@ Because the proof should follow the recursive structure of the program, you may 
 copy the termination argument from the program to the proof.
 -/
 theorem filter_go_all [DecidablePred p] (hAcc : All p acc)
-    : All p (filter.go p xs i acc) := by
-  unfold filter.go
-  split
-  · split
-    . apply filter_go_all
-      apply push_all
-      . assumption
-      . assumption
-    . apply filter_go_all
-      . assumption
-  · assumption
-termination_by xs.size - i
+    : All p (filter.go p xs i acc) := sorry
 
 
-theorem filter_all (p : α → Prop) [DecidablePred p] : All p (filter p xs) := by
-  simp [filter, filter_go_all]
+theorem filter_all (p : α → Prop) [DecidablePred p] : All p (filter p xs) := by sorry
 
 
 /-
@@ -91,8 +70,6 @@ also makes it easier to write a highly automated proof that will be more robust 
 changes to `filter`.
 -/
 theorem filter_go_all' [DecidablePred p] (hAcc : All p acc)
-    : All p (filter.go p xs i acc) := by
-  induction i, acc using filter.go.induct p xs <;> unfold filter.go <;> simp [*]
+    : All p (filter.go p xs i acc) := by sorry
 
-theorem filter_all' (p : α → Prop) [DecidablePred p] : All p (filter p xs) := by
-  simp [filter, filter_go_all']
+theorem filter_all' (p : α → Prop) [DecidablePred p] : All p (filter p xs) := by sorry
