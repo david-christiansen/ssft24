6b668c63daacda3e3a07e8fec27dc6bbf1ea96bb
David Thrane Christiansen <david@davidchristiansen.dk>
2024-05-26 21:04:14 -0700

    Intro setup

--- Intro.lean
+++ Intro.lean
@@ -2,47 +2,44 @@
 -- Programming in Lean --
 -------------------------
 
-def double (n : Nat) : Nat :=
-  match n with
-  | 0 => 0
-  | n' + 1 => double n' + 2
 
-#eval double 5
+def double (n : Nat) : Nat := sorry
 
-#check double
 
-def double' : Nat → Nat
-  | 0 => 0
-  | n' + 1 => double' n' + 2
+-- #eval double 5
 
-def multiply (k : Nat) : Nat → Nat
-  | 0 => 0
-  | n' + 1 => multiply k n' + k
 
-def append : List α → List α → List α
-  | [], ys => ys
-  | x :: xs, ys => x :: append xs ys
+-- #check double
+
+
+def double' : Nat → Nat := sorry
+
+
+def multiply (k : Nat) : Nat → Nat := sorry
+
+
+def append : List α → List α → List α := sorry
+
 
 inductive Tree (α : Type) where
-  | leaf : Tree α
-  | branch : Tree α → α → Tree α → Tree α
 
-def Tree.toList : Tree α → List α
-  | leaf => []
-  | branch l x r => l.toList ++ [x] ++ r.toList
 
-#eval (Tree.branch (Tree.branch Tree.leaf 1 Tree.leaf) 2 Tree.leaf).toList
+def Tree.toList : Tree α → List α := sorry
+
+
+-- #eval (Tree.branch (Tree.branch Tree.leaf 1 Tree.leaf) 2 Tree.leaf).toList
 
 
 inductive Even : Nat → Prop where
-  | isEven : (half : Nat) → Even (half + half)
 
-example : Even 6 := .isEven 3
+
+-- example : Even 6 := .isEven 3
+
 
 inductive Repeats (x : α) : List α → Prop where
-  | nil : Repeats x []
-  | cons : Repeats x xs → Repeats x (x :: xs) -- here xs is implicitly an argument
 
-example : Repeats 3 [3,3,3] := .cons (.cons (.cons .nil))
 
-def Both (p q : α → Prop) : α → Prop := fun x => p x ∧ q x
+-- example : Repeats 3 [3,3,3] := .cons (.cons (.cons .nil))
+
+
+def Both (p q : α → Prop) : α → Prop := sorry
